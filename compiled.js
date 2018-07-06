5ccd45f8759a2038ad6c79bb47d887de6947b585
sreenishd <sreenishd@gmail.com>
2018-07-06 13:49:09 +0200

    modified

---
+++ compiled.js
@@ -0,0 +1,87 @@
+const CREATE = 'CREATE';
+const REMOVE = 'REMOVE';
+const REPLACE = 'REPLACE';
+const UPDATE = 'UPDATE';
+const SET_PROP = 'SET_PROP';
+const REMOVE_PROP = 'REMOVE PROP';
+
+//// DIFF
+
+function changed(node1, node2) {}
+
+function diffProps(newNode, oldNode) {}
+
+function diffChildren(newNode, oldNode) {}
+
+function diff(newNode, oldNode) {} //@
+
+//// PATCH
+
+function createElement(node) {
+    //@
+    if (typeof node === 'string') {
+        return document.createTextNode(node);
+    }
+    const el = document.createElement(node.type);
+    setProps(el, node.props);
+    node.children.map(createElement).forEach(el.appendChild.bind(el));
+    return el;
+}
+
+function setProp(target, name, value) {
+    //@
+    if (name == 'className') {
+        return target.setAttribute('class', value);
+    }
+    target.setAttribute(name, value);
+}
+
+function setProps(target, props) {
+    if (props) {
+        Object.keys(props).forEach(name => {
+            setProp(target, name, props[name]);
+        });
+    }
+}
+
+function removeProp(target, name, value) {//@
+
+}
+
+function patchProps(parent, patches) {}
+
+function patch(parent, patches, index = 0) {} //@
+
+//// My Application
+
+function flatten(arr) {
+    return [].concat.apply([], arr);
+}
+
+function h(type, props, ...children) {
+    return { type, props, children: flatten(children) };
+}
+
+function view(count) {
+    //@
+    return h(
+        'ul',
+        { li: 'cool', className: 'foo' },
+        h(
+            'li',
+            { className: 'test' },
+            'test1'
+        ),
+        h(
+            'li',
+            null,
+            'test2'
+        )
+    );
+}
+
+function tick(el, count) {}
+
+function render(el) {
+    el.appendChild(createElement(view(0)));
+}
--- index.js
+++ index.js
@@ -26,15 +26,28 @@ function diff(newNode, oldNode) {  //@
 //// PATCH
 
 function createElement(node) { //@
-
+    if(typeof node==='string'){
+        return document.createTextNode(node);
+    }
+    const el = document.createElement(node.type);
+    setProps(el,node.props)
+    node.children.map(createElement).forEach(el.appendChild.bind(el))
+    return el;
 }
 
 function setProp(target, name, value) { //@
-
+    if(name == 'className'){
+        return target.setAttribute('class',value);
+    }
+    target.setAttribute(name,value);
 }
 
 function setProps(target, props) {
-
+    if(props){
+        Object.keys(props).forEach(name=>{
+            setProp(target,name,props[name])
+        })
+    }
 }
 
 function removeProp(target, name, value) { //@
@@ -52,15 +65,18 @@ function patch(parent, patches, index = 0) { //@
 //// My Application
 
 function flatten(arr) {
-
+    return [].concat.apply([],arr);
 }
 
 function h(type, props, ...children) {
-
+    return{ type,props,children:flatten(children)}
 }
 
 function view(count) { //@
-
+    return <ul li="cool" className="foo">
+        <li className="test">test1</li>
+        <li>test2</li>
+    </ul>
 }
 
 function tick(el, count) {
@@ -68,5 +84,5 @@ function tick(el, count) {
 }
 
 function render(el) {
-
+    el.appendChild(createElement(view(0)))
 }
